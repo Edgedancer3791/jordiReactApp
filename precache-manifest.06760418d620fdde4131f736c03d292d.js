3d6af8b0f3161c07e6b83ec141fbc787842d81da
Edgedancer3791 <jordiprimsvicario@gmail.com>
2020-09-24 17:44:43 +0200

    Updates

---
+++ precache-manifest.06760418d620fdde4131f736c03d292d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d2dc21e4e024fe76e5b2190a1c6605",
+    "url": "/jordiReactApp/index.html"
+  },
+  {
+    "revision": "4716a058f8916403a165",
+    "url": "/jordiReactApp/static/css/main.3fd2f8de.chunk.css"
+  },
+  {
+    "revision": "21d81c4fd770ea351271",
+    "url": "/jordiReactApp/static/js/2.39f74a35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jordiReactApp/static/js/2.39f74a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4716a058f8916403a165",
+    "url": "/jordiReactApp/static/js/main.7334e2e2.chunk.js"
+  },
+  {
+    "revision": "4a1644d053ab6f208c87",
+    "url": "/jordiReactApp/static/js/runtime-main.a1869f04.js"
+  }
+]);
